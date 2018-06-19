d844527babecee54f04447ecaa7b94ffb618c745
Ludwig Nussel <ludwig.nussel@suse.de>
2018-06-19 17:04:38 +0200

    Some fixups

--- lib/opensuse.sh
+++ lib/opensuse.sh
@@ -1,7 +1,11 @@
 bonsai_install()
 {
 	mkdir -p "$dest"/etc/zypp/repos.d
-	zypper --root "$dest" ar -f "$repo_oss_url" repo-oss
+	: ${repos:=oss}
+	for repo in "${repos[@]}"; do
+		eval url=\"\$repo_${repo}_url\"
+		zypper --root "$dest" ar -f "$url" repo-$repo
+	done
 	rpm --root "$dest" --import /usr/lib/rpm/gnupg/keys/*
 	if [ -n "$copy_fillup_passwd" ]; then
 		cp /var/adm/fillup-templates/passwd.aaa_base "$dest"/etc/passwd
--- profiles/leap423.sh
+++ profiles/leap423.sh
@@ -1,5 +1,4 @@
 #!/bin/sh
 base=opensuse
 repo_oss_url="http://download.opensuse.org/distribution/leap/42.3/repo/oss"
-copy_fillup_passwd=y
-default_packages="patterns-openSUSE-base patterns-openSUSE-sw_management"
+default_packages="patterns-openSUSE-base"
