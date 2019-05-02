a8800b987f147e517a9c4b09638412a7a2e08e4f
Ludwig Nussel <ludwig.nussel@suse.de>
2019-05-02 11:28:15 +0200

    add leap 15.1

---
+++ profiles/leap151.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+base=opensuse
+repos=(oss oss_update nonoss nonoss_update)
+repo_oss_url="http://download.opensuse.org/distribution/leap/15.1/repo/oss"
+repo_oss_update_url="http://download.opensuse.org/update/leap/15.1/oss/"
+repo_nonoss_url="http://download.opensuse.org/distribution/leap/15.1/repo/non-oss"
+repo_nonoss_update_url="http://download.opensuse.org/update/leap/15.1/non-oss/"
+default_packages="patterns-base-base"
